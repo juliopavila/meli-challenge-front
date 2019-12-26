f686dc7ea56d937bfbc84fc6edfd83f411cf0ba8
Julio Avila <javila1997@gmail.com>
2019-12-25 23:02:51 -0300

    Dinamic ports

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
 import http from "axios";
 const baseUrl = "https://meli-backend.herokuapp.com/api/items";
+const localUrl = "http://localhost:3100/";
 
 export function getProductsApi(query) {
-  return http.get(`${baseUrl}?query=${query}`);
+  return http.get(`${baseUrl}?query=${query}` || `${localUrl}?query=${query}`);
 }
 
 export function getProdDetailsApi(id) {
-  return http.get(`${baseUrl}/${id}`);
+  return http.get(`${baseUrl}/${id}` || `${localUrl}/${id}`);
 }
